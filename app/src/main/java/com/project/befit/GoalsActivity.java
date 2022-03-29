40737a5e4a4a47f16707d94d70ec9e20a8815a4d
braydenc11599 <clarkb24@lsus.edu>
2022-03-29 14:40:53 -0500

    Making commit and push so I can access my goals screen changes on my desktop.

--- app/src/main/java/com/project/befit/GoalsActivity.java
+++ app/src/main/java/com/project/befit/GoalsActivity.java
@@ -1,14 +1,19 @@
 package com.project.befit;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import com.example.befit.R;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class GoalsActivity extends AppCompatActivity {
+public class GoalsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Button logoutB;
     FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
@@ -18,8 +23,19 @@ public class GoalsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals);
 
-        logoutB = findViewById(R.id.buttonLogout);
+        Spinner spinner1 = findViewById(R.id.sexSpinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.sex_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(this);
 
+        Spinner spinner2 = findViewById(R.id.activityLevelSpinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.activityLevel_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+        logoutB = findViewById(R.id.buttonLogout);
         logoutB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +44,16 @@ public class GoalsActivity extends AppCompatActivity {
                 startActivity(intToMain);
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(GoalsActivity.this,"Something Went Wrong, Please Select An Item From The List",Toast.LENGTH_SHORT).show();
     }
 }
